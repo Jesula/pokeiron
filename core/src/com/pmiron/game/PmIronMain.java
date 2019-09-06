151cd25921c4c540deab843b7fd3212193ab2664
Jesse Maynard <kurumu63@Jesses-MacBook-Pro.local>
2019-09-06 02:06:48 -0400

    rudementary movement

--- core/src/com/pmiron/game/PmIronMain.java
+++ core/src/com/pmiron/game/PmIronMain.java
@@ -2,6 +2,7 @@ package com.pmiron.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -9,19 +10,33 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class PmIronMain extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	
+
+	float x = 0;
+	float y = 0;
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("scalder_test2.png");
 	}
 
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		if (Gdx.input.isKeyPressed(Input.Keys.W)){
+			y = y + 2;
+		} else if (Gdx.input.isKeyPressed(Input.Keys.S)){
+			y = y - 2;
+		} else if (Gdx.input.isKeyPressed(Input.Keys.A)){
+			x = x - 2;
+		} else if (Gdx.input.isKeyPressed(Input.Keys.D)){
+			x = x + 2;
+		}
+
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(img, x, y);
 		batch.end();
 	}
 	
