40e6563cae22e597df4b6f07a2bf84f81b774dfe
Jesse Maynard <kurumu63@Jesses-MacBook-Pro.local>
2019-09-08 20:15:12 -0400

    walk cycle sprites added

---
+++ core/src/com/pmiron/util/TexturePackerTool.java
@@ -0,0 +1,12 @@
+package com.pmiron.util;
+
+import com.badlogic.gdx.tools.texturepacker.TexturePacker;
+
+public class TexturePackerTool {
+
+    public static void main(String[] args) {
+//        TexturePacker.process();
+                
+    }
+
+}
